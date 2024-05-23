98762f7bc647b1f493c50e0dd210609421d5cee8
Alex Uscata <alex.uscata@yoo.digital>
2024-05-23 19:53:50 +0200

    sigma

--- src/scripts/components/map.ts
+++ src/scripts/components/map.ts
@@ -1,32 +1,47 @@
-// Initialize and add the map
-let map;
-async function initMap(): Promise<void> {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+import { Loader } from "@googlemaps/js-api-loader";
 
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-
-  // The map, centered at Uluru
-  map = new Map(
-    document.getElementById('map') as HTMLElement,
-    {
-      zoom: 4,
-      center: position,
-      mapId: 'DEMO_MAP_ID',
-    }
-  );
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: 'Uluru'
+export const init = () => {
+  const loader = new Loader({
+    apiKey: "AIzaSyDErYHyA-Qn6QkXqS6lCxkpk_-jWX_4-fQ",
+    version: "weekly",
   });
-}
+  loader.load().then(async () => {
+    // In this example, we center the map, and add a marker, using a LatLng object
+    // literal instead of a google.maps.LatLng object. LatLng object literals are
+    // a convenient way to add a LatLng coordinate and, in most cases, can be used
+    // in place of a google.maps.LatLng object.
+    let map: google.maps.Map;
 
-export const init = (rootEl: HTMLElement) => {
-  initMap();
+      const mapOptions = {
+        zoom: 20,
+        center: { lat: 37.51457587272004, lng: 127.10249569677539 }
+      };
+
+      map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        mapOptions
+      );
+      const marker = new google.maps.Marker({
+        // The below line is equivalent to writing:
+        // position: new google.maps.LatLng(-34.397, 150.644)
+        position: { lat: 37.51457587272004, lng: 127.10249569677539 },
+        map: map,
+      });
+
+      // You can use a LatLng literal in place of a google.maps.LatLng object when
+      // creating the Marker object. Once the Marker object is instantiated, its
+      // position will be available as a google.maps.LatLng object. In this case,
+      // we retrieve the marker's position using the
+      // google.maps.LatLng.getPosition() method.
+      const infowindow = new google.maps.InfoWindow({
+        // content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+        content: "<p> ONLY SIGMAS CAN ENTER </p>",
+      });
+
+      google.maps.event.addListener(marker, "click", () => {
+        infowindow.open(map, marker);
+      });
+  }).catch(error => {
+    console.error('Fehler beim Laden der Google Maps API:', error);
+  });
 };
